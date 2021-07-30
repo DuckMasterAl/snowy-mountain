c19ca2a5656cff922ad0abfd7329e74aa9dc17ab
DuckMasterAl <duckmaster.dev@gmail.com>
2021-07-30 16:10:40 -0400

    Suggestions and Selfrole updates
    - The selfrole code is no longer spaghetti code
    - Selfroles now use selects
    - Selfroles are now cached
    - Suggestion and suggestion management exists

--- cogs/events.py
+++ cogs/events.py
@@ -1,6 +1,34 @@
 import discord, datetime, message_embeds, math
 from discord.ext import commands, tasks
-from discord_components import DiscordComponents, Button, ButtonStyle
+from discord_components import DiscordComponents, Button, ButtonStyle, Select, SelectOption
+
+async def update_selfrole_interaction(self, interaction, components, member_roles):
+    buttons = []
+    clicked_buttons = 0
+    for a in components:
+        if str(a) in member_roles:
+            clicked_buttons += 1
+            button_style = ButtonStyle.blue
+        else:
+            button_style = ButtonStyle.grey
+        buttons.append(Button(style=button_style, id=f"ROLE_TOGGLE-{a}", label=interaction.guild.get_role(int(a)).name))
+
+    if clicked_buttons == len(buttons):
+        for b in buttons:
+            b.style = ButtonStyle.green
+
+    buttons = sorted(buttons, key=lambda q: q.label)
+    total_buttons = []
+    for c in range(math.ceil(len(buttons) / 5)):
+        total_buttons.append(buttons[:5])
+        buttons = buttons[5:]
+
+    selects = []
+    for x in self.bot.selfroles:
+        selects.append(SelectOption(label=x['name'], value=f"ROLE_LIST-{x['name']}"))
+    total_buttons = total_buttons + [Select(placeholder="Switch role categories", options=selects)]
+
+    await interaction.respond(type=7, content="Please select the roles you want.", components=total_buttons)
 
 class Events(commands.Cog):
     def __init__(self, bot):
@@ -8,12 +36,14 @@ class Events(commands.Cog):
         self.member_role = 866019665502273586
         self.serious_access_role = 866362223000289310
         self.news_role = 866023627497406495
-        self.suggestion_channel = 865920296660500552
         self.serious_channels = [865918494791827536, 865918534177783808]
+        self.introduction_channel = 866381489989222410
         self.clear_serious_chats.start()
+        self.recache_selfroles.start()
 
     def cog_unload(self):
         self.clear_serious_chats.cancel()
+        self.recache_selfroles.cancel()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -41,17 +71,15 @@ class Events(commands.Cog):
             channel = self.bot.get_channel(x)
             await channel.purge(limit=None, before=(datetime.datetime.now() - datetime.timedelta(days=7)), check=lambda k: not k.pinned)
 
+    @tasks.loop(hours=12)
+    async def recache_selfroles(self):
+        await self.bot.wait_until_ready()
+        self.bot.selfroles = await self.bot.db.roles.find({}).to_list(None)
+
     @commands.Cog.listener('on_message')
-    async def suggestion(self, message):
-        if message.channel.id != self.suggestion_channel or message.author.bot:
-            return
-        embed = discord.Embed(title="New Suggestion", description=str(message.content), color=discord.Color.blue())
-        embed.set_author(name=str(message.author.display_name), icon_url=str(message.author.avatar_url), url=f"https://discordrep.com/u/{message.author.id}#")
-        await message.delete()
-        msg = await message.channel.send(embed=embed)
-        await msg.add_reaction(self.bot.get_emoji(866363506071437352))
-        await msg.add_reaction(self.bot.get_emoji(865733569003782194))
-        await msg.add_reaction(self.bot.get_emoji(866348776851111936))
+    async def introduction_reactions(self, message):
+        if message.channel.id == self.introduction_channel and message.author.bot is False:
+            await message.add_reaction(self.bot.get_emoji(865718455684562946))
 
     @commands.Cog.listener('on_message')
     async def feedback_dms(self, message):
@@ -61,7 +89,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        embed = discord.Embed(title='You broke it <:foxREE:826881122445033549>', description=str(error), color=discord.Color.red())
+        embed = discord.Embed(title='You broke it <:foxunamused:865733585600249876>', description=str(error), color=discord.Color.red())
         await ctx.send(embed=embed)
 
     @commands.Cog.listener()
@@ -88,49 +116,40 @@ class Events(commands.Cog):
                 await interaction.respond(content="You will now be pinged for updates in <#865915066032062464> <:dogping:866348393893724160>")
         elif interaction.component.id == ("LIST_ROLES"):
             options = await self.bot.db.roles.find({}).to_list(None)
-            buttons = []
+            selects = []
             for x in options:
-                buttons.append(Button(label=x['name'], id=f"ROLE_LIST-{x['name']}"))
-            await interaction.respond(content="Which category of roles would you like to view?", components=[buttons])
-        elif interaction.component.id.startswith("ROLE_LIST-"):
-            data = await self.bot.db.roles.find_one({"name": interaction.component.id.split('-')[1]})
-            roles = data['roles']
-            buttons = []
-            for x in roles:
-                role = interaction.guild.get_role(int(x))
-                buttons.append(Button(label=str(role.name), id=f"ROLE_TOGGLE-{role.id}"))
-
-            member_roles = [str(b.id) for b in self.bot.get_guild(int(interaction.guild.id)).get_member(int(interaction.user.id)).roles]
-            for a in buttons:
-                if a.id.split('-')[1] in member_roles:
-                    a.style = ButtonStyle.blue
-
-            buttons = sorted(buttons, key=lambda q: q.label)
-            total_buttons = []
-            for x in range(math.ceil(len(buttons) / 5)):
-                total_buttons.append(buttons[:5])
-                buttons = buttons[5:]
-            await interaction.respond(content="Please select the roles you want.", components=total_buttons)
+                selects.append(SelectOption(label=x['name'], value=f"ROLE_LIST-{x['name']}"))
+            await interaction.respond(content="Select the role category from the dropdown below.", components=[Select(placeholder="Select the category of roles you want to view.", options=selects)])
         elif interaction.component.id.startswith("ROLE_TOGGLE-"):
             member = self.bot.get_guild(int(interaction.guild.id)).get_member(int(interaction.user.id))
             role = interaction.guild.get_role(int(interaction.component.id.split('-')[1]))
+            member_roles = [str(b.id) for b in member.roles]
             if role not in member.roles:
                 await member.add_roles(role, reason="Pushed a Toggle Role Button")
+                if str(role.id) not in member_roles:
+                    member_roles.append(str(role.id))
             else:
                 await member.remove_roles(role, reason="Pushed a Toggle Role Button")
+                try:
+                    member_roles.remove(str(role.id))
+                except ValueError:
+                    pass
 
-            components = (await self.bot.db.roles.find_one({"roles": {"$in": [str(role.id)]}}))['roles']
-            member_roles = [str(b.id) for b in member.roles]
-            buttons = []
-            for a in components:
-                buttons.append(Button(style=ButtonStyle.blue if str(a) in member_roles else ButtonStyle.grey, id=f"ROLE_TOGGLE-{a}", label=interaction.guild.get_role(int(a)).name))
+            for x in self.bot.selfroles:
+                if str(role.id) in x['roles']:
+                    selfroles = x['roles']
 
-            buttons = sorted(buttons, key=lambda q: q.label)
-            total_buttons = []
-            for x in range(math.ceil(len(buttons) / 5)):
-                total_buttons.append(buttons[:5])
-                buttons = buttons[5:]
-            await interaction.respond(type=7, content="Please select the roles you want.", components=total_buttons)
+            await update_selfrole_interaction(self, interaction, selfroles, member_roles)
+
+    @commands.Cog.listener()
+    async def on_select_option(self, interaction):
+        if interaction.component.value.startswith("ROLE_LIST-"):
+            for x in self.bot.selfroles:
+                if x['name'] == interaction.component.value.split('-')[1]:
+                    selfroles = x['roles']
+
+            member_roles = [str(b.id) for b in (self.bot.get_guild(int(interaction.guild.id)).get_member(int(interaction.user.id))).roles]
+            await update_selfrole_interaction(self, interaction, selfroles, member_roles)
 
 def setup(bot):
     bot.add_cog(Events(bot))
---
+++ cogs/suggestions.py
@@ -0,0 +1,104 @@
+import discord, datetime, message_embeds
+from discord.ext import commands, tasks
+from discord_components import Button, ButtonStyle
+from discord_slash import cog_ext
+from discord_slash.utils.manage_commands import create_permission
+from discord_slash.model import SlashCommandPermissionType
+
+async def update_suggestion(self, ctx, suggestion_id, comment, color, action):
+    suggestion = await self.bot.db.suggestions.find_one({"id": str(suggestion_id)})
+    if suggestion is None:
+        return await ctx.send("That is not a valid Suggestion ID!", hidden=True)
+    msg = await self.bot.get_channel(self.suggestion_channel).fetch_message(int(suggestion['message']))
+    embed = msg.embeds[0]
+    embed.color = color
+    embed.title = f"{action.capitalize()} Suggestion (#{suggestion['id']})"
+    if comment is not None:
+        embed.add_field(name=f"Comment from {ctx.author.display_name}", value=str(comment), inline=False)
+    await msg.edit(embed=embed)
+    user = self.bot.get_user(int(suggestion['user']))
+    if user is not None:
+        try:
+            await user.send(f"Your suggestion has been {action.lower()} by {ctx.author.display_name}!", embed=embed)
+        except:
+            pass
+    await msg.clear_reactions()
+    await ctx.send(f"{action.capitalize()} Suggestion {suggestion_id}!", embed=embed, hidden=True)
+
+class Suggestions(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.suggestion_channel = 865920296660500552
+        self.approve_emoji = self.bot.get_emoji(866363506071437352)
+        self.shrug_emoji = self.bot.get_emoji(865733569003782194)
+        self.deny_emoji = self.bot.get_emoji(866348776851111936)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id != self.suggestion_channel or message.author.bot:
+            return
+        suggestion_id = str(len(await self.bot.db.suggestions.find({}).to_list(None)) + 1)
+        embed = discord.Embed(title=f"New Suggestion (#{suggestion_id})", description=str(message.content), color=discord.Color.blue())
+        embed.set_author(name=str(message.author.display_name), icon_url=str(message.author.avatar_url), url=f"https://discordrep.com/u/{message.author.id}#")
+        await message.delete()
+        msg = await message.channel.send(embed=embed)
+        await self.bot.db.suggestions.insert_one({"id": suggestion_id, "user": str(message.author.id), "message": str(msg.id)})
+        await msg.add_reaction(self.approve_emoji)
+        await msg.add_reaction(self.shrug_emoji)
+        await msg.add_reaction(self.deny_emoji)
+
+    @cog_ext.cog_subcommand(base="suggestion", name="approve", description="Approve a suggestion!", guild_ids=[865711881075163166], base_default_permission=False,
+    options=[{"name": "suggestion_id", "description": "The id of the suggestion.", "type": 4, "required": True},
+    {"name": "comment", "description": "A suggestion comment to be added when approving.", "type": 3, "required": False}])
+    @cog_ext.permission(guild_id=865711881075163166, permissions=[create_permission(865905302003646484, SlashCommandPermissionType.ROLE, True)])
+    async def suggestion_approve(self, ctx, suggestion_id, comment=None):
+        await update_suggestion(self, ctx, suggestion_id, comment, discord.Color.green(), 'approved')
+
+    @cog_ext.cog_subcommand(base="suggestion", name="deny", description="Deny a suggestion!", guild_ids=[865711881075163166], base_default_permission=False,
+    options=[{"name": "suggestion_id", "description": "The id of the suggestion.", "type": 4, "required": True},
+    {"name": "comment", "description": "A suggestion comment to be added when approving.", "type": 3, "required": False}])
+    @cog_ext.permission(guild_id=865711881075163166, permissions=[create_permission(865905302003646484, SlashCommandPermissionType.ROLE, True)])
+    async def suggestion_deny(self, ctx, suggestion_id, comment=None):
+        await update_suggestion(self, ctx, suggestion_id, comment, discord.Color.red(), 'denied')
+
+    @cog_ext.cog_subcommand(base="suggestion", name="implemented", description="Mark a suggestion as implemented!", guild_ids=[865711881075163166], base_default_permission=False,
+    options=[{"name": "suggestion_id", "description": "The id of the suggestion.", "type": 4, "required": True},
+    {"name": "comment", "description": "A suggestion comment to be added when denying.", "type": 3, "required": False}])
+    @cog_ext.permission(guild_id=865711881075163166, permissions=[create_permission(865905302003646484, SlashCommandPermissionType.ROLE, True)])
+    async def suggestion_implemented(self, ctx, suggestion_id, comment=None):
+        await update_suggestion(self, ctx, suggestion_id, comment, discord.Color.light_magenta(), 'implemented')
+
+    @cog_ext.cog_subcommand(base="suggestion", name="inprogress", description="Mark a suggestion as in progress!", guild_ids=[865711881075163166], base_default_permission=False,
+    options=[{"name": "suggestion_id", "description": "The id of the suggestion.", "type": 4, "required": True},
+    {"name": "comment", "description": "A suggestion comment to be added when marking as in progress.", "type": 3, "required": False}])
+    @cog_ext.permission(guild_id=865711881075163166, permissions=[create_permission(865905302003646484, SlashCommandPermissionType.ROLE, True)])
+    async def suggestion_inprogress(self, ctx, suggestion_id, comment=None):
+        suggestion = await self.bot.db.suggestions.find_one({"id": str(suggestion_id)})
+        if suggestion is None:
+            return await ctx.send("That is not a valid Suggestion ID!", hidden=True)
+        msg = await self.bot.get_channel(self.suggestion_channel).fetch_message(int(suggestion['message']))
+        embed = msg.embeds[0]
+        embed.color = discord.Color.dark_yellow()
+        embed.title = f"In Progress Suggestion (#{suggestion['id']})"
+        if comment is not None:
+            embed.add_field(name=f"Comment from {ctx.author.display_name}", value=str(comment), inline=False)
+        await msg.edit(embed=embed)
+        await msg.clear_reactions()
+        await ctx.send(f"Marked Suggestion {suggestion_id} as in progress!", embed=embed, hidden=True)
+
+    @cog_ext.cog_subcommand(base="suggestion", name="comment", description="Add a comment to a suggestion!", guild_ids=[865711881075163166],
+    options=[{"name": "suggestion_id", "description": "The id of the suggestion.", "type": 4, "required": True},
+    {"name": "comment", "description": "The comment you'd like to add to the suggestion.", "type": 3, "required": True}])
+    @cog_ext.permission(guild_id=865711881075163166, permissions=[ create_permission(865905302003646484, SlashCommandPermissionType.ROLE, True)])
+    async def suggestion_comment(self, ctx, suggestion_id, comment):
+        suggestion = await self.bot.db.suggestions.find_one({"id": str(suggestion_id)})
+        if suggestion is None:
+            return await ctx.send("That is not a valid Suggestion ID!", hidden=True)
+        msg = await self.bot.get_channel(self.suggestion_channel).fetch_message(int(suggestion['message']))
+        embed = msg.embeds[0]
+        embed.add_field(name=f"Comment from {ctx.author.display_name}", value=str(comment), inline=False)
+        await msg.edit(embed=embed)
+        await ctx.send(f"Added a comment to suggestion {suggestion_id}!", embed=embed, hidden=True)
+
+def setup(bot):
+    bot.add_cog(Suggestions(bot))
